55f6cbbf0a0be8579607d1928ebf214cded67d42
nelson sakhile <nelsonsakhile@gmail.com>
2024-11-13 16:43:59 +0200

    Set up SQL lite DB

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask import redirect
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return None
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000, debug=True)
--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,11 @@
-from flask import flask
-from flask_sqlachemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = FLASK(__name__)
+app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqllite://mydatabase.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
