0a0987f13a37ee44dd99ba02694823b7a0a875a8
nelson sakhile <nelsonsakhile@gmail.com>
2024-12-04 12:29:12 +0200

    Fix Modeld & routes

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 from app import database as db
+import datetime
 
 class Role(db.Model):
   __tablename__ = "roles"
@@ -15,21 +16,21 @@ class User(db.Model):
   surname = db.Column(db.Text, nullable=False)
   
   def __repr__(self):
-    return f"Name {self.name}, \n surname {self.surname}, \n employee_id {self.employee_id}"
+    return f"Name {self.name}, surname {self.surname}, employee_id {self.employee_id}"
 
 # For Crew members
 
-#class Depature(db.Model):
-  #id = db.Column(db.Integer, primary_key=True)
- # date = date
-#  day = sun-sat
-
-#class Return(db.Model):
- # id = db.Column(db.Integer, primary_key=True)
- # date = date
- # day = day
- # time = time
-  
+class TripDetails(db.Model):
+  __tablename__ = "trips"
+  id = db.Column(db.Integer, primary_key=True)
+  location = db.Column(db.Text, nullable=False)
+  driver = db.Column(db.Text, nullable=False)
+  day_depature = db.Column(db.Text, nullable=False)
+  day_return = db.Column(db.Text, nullable=False, default= "Friday")
+  date_depature = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+  date_return = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow())
+  def __repr__(self):
+    return f"location: {self.location}, \n driver: {self.driver}"
   
 # For Dispatch gents/Slaves
 
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, request
-from models import User
+from flask import render_template, jsonify, request
+from models import User, TripDetails
 
 def query_routes(app, database):
   @app.route("/") 
@@ -12,6 +12,7 @@ def query_routes(app, database):
     name = request.form.get("name")
     surname = request.form.get("surname")
     user = User(name=name, surname=surname)
+    
     database.session.add(user)
     database.session.commit()
     employees = User.query.all()
@@ -19,4 +20,20 @@ def query_routes(app, database):
     
   @app.route("/add_trip", methods = ["POST"])
   def trip_details():
-    pass
+    location = request.form.get("location")
+    driver = request.form.get("driver")
+    day_depature = request.form.get("day_depature")
+    day_return = request.form.get("day_return")
+    
+    trip = TripDetails(
+      location = location, 
+      driver = driver, 
+      day_depature = day_depature,
+      day_return = day_return,
+      )
+      
+    database.session.add(trip)
+    database.session.commit()
+    trips = TripDetails.query.all()
+    return str(trip)
+    
